c0dc81ecc47a18f73a9f7a97cdb448814236a456
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2023-01-03 21:29:58 +0700

    add benchmark

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<Benchmark>();
+
+[MemoryDiagnoser(false)]
+public class Benchmark
+{
+    private List<int> _list;
+    private HashSet<int> _hashset;
+
+    [Params(10_000)]
+    public int Count { get; set; }
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        IEnumerable<int> range = Enumerable.Range(0, Count);
+
+        _list = range.ToList();
+        _hashset = new(range);
+    }
+
+    [Benchmark]
+    public int List_Count_Property() => _list.Count;
+    [Benchmark]
+    public int List_Count_Method() => _list.Count();
+
+    [Benchmark]
+    public int HashSet_Count_Property() => _hashset.Count;
+    [Benchmark]
+    public int HashSet_Count_Method() => _hashset.Count();
+}
